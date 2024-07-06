d111e87595fd2c543a1776383decda7be89186cf
Ваня Науменко <ivannaumenko@MacBook-Pro-Vana.local>
2024-07-06 15:23:05 +0300

    add project

---
+++ TestReset/TestReset/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestReset
+//
+//  Created by Ваня Науменко on 6.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestReset/TestReset/TestResetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestResetApp.swift
+//  TestReset
+//
+//  Created by Ваня Науменко on 6.07.24.
+//
+
+import SwiftUI
+
+@main
+struct TestResetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
